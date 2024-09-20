e80fb19d67d52af359e3917fdca4a1179e88b40b
mickck <mikemwkim@gmail.com>
2024-09-21 01:52:11 +1000

    send all code

--- app/page.js
+++ app/page.js
@@ -1,101 +1,41 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export const API_URL = "https://billions-api.nomadcoders.workers.dev";
+export const metadata = {
+  title: "Buja list Home",
+  description: "Find Buja list",
+};
+async function getRiches() {
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+export default async function Home() {
+  const riches = await getRiches();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="container">
+      <h1 className="mb-8">Buja List</h1>
+      <div className="flex flex-wrap gap-3 jc justify-center">
+        {riches.map((rich) => (
+          <Link
+            key={rich.id}
+            href={`/person/${rich.id}`}
+            className="rounded-md border border-gray-400  mb-5 w-[48%] md:w-[30%] lg:w-[20%] hover:bg-white hover:text-black transition-all"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <img
+              src={rich.squareImage}
+              height={300}
+              width={300}
+              className="rounded-t-md"
+            ></img>
+            <p className="text-center mt-2 mb-1 font-semibold">{rich.name}</p>
+            <p className="text-center mb-2 text-sm opacity-80">
+              {rich.industries[0]}, {rich.netWorth}
+            </p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ app/person/[rich]/page.js
@@ -0,0 +1,75 @@
+import { API_URL } from "../../page";
+import style from "./rich.module.css";
+
+export default async function RichPerson({ params }) {
+  async function getRich() {
+    const response = await fetch(`${API_URL}/person/${params.rich}`);
+    const json = await response.json();
+    return json;
+  }
+  const rich = await getRich();
+
+  return (
+    <div className={style.container}>
+      <h1 className="mb-3 font-semibold text-xl ">{rich.name}</h1>
+      <div className="md:flex justify-between">
+        <div className="md:w-[40%]">
+          <img
+            src={rich.squareImage}
+            height={300}
+            width={300}
+            className="rounded-md mb-4 w-64 md:w-max"
+          ></img>
+        </div>
+        <div className="md:w-[55%] md:text-lg">
+          <p className="mb-1">
+            <b>Where: </b>
+            {rich.city} {rich.state} {rich.country}
+          </p>
+          <p className="mb-1">
+            <b>Industries:</b>{" "}
+            {rich.industries.map((element, index) => (
+              <span className="text-black" key={index}>
+                {element}
+              </span>
+            ))}
+          </p>
+          <p className="font-bold md:text-lg">Story</p>
+          <p className="text-sm mb-1">{rich.bio}</p>
+        </div>
+      </div>
+      <p className="font-bold my-2">Assets</p>
+      <div className="flex flex-wrap justify-between">
+        {rich.financialAssets.map((element, index) => (
+          <div
+            className="text-sm p-2 md:p-3 bg-gray-200 rounded-md mb-4 w-[100%] md:w-[48%] flex flex-col gap-1"
+            key={index}
+          >
+            <b className="text-md mb-1">{element.exchange}</b>
+            <p>
+              <b>Ticker</b>: {element.ticker}
+            </p>
+            <p>
+              <b>Company Name</b>: {element.companyName}
+            </p>
+            <p>
+              <b>Number of Shares</b>: {element.numberOfShares}
+            </p>
+            <p>
+              <b>Currency Code</b>: {element.currencyCode}
+            </p>
+            <p>
+              <b>Exchange Rate</b>: {element.exchangeRate}
+            </p>
+            <p>
+              <b>Interactive</b>: {element.interactive}
+            </p>
+            <p>
+              <b>CurrentPrice</b>: {element.currentPrice}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
